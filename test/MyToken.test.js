fa429e7cb1052b533a8501ee621c0f0627354d82
jake-and-stake <0xvanquisher@gmail.com>
2022-10-19 16:30:56 -0700

    Add beforeEach call to unit test.

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -16,14 +16,18 @@ contract("MyToken test", async accounts => {
 
     const [deployerAccount, recipient] = accounts;
 
+    beforeEach(async() => {
+        this.myToken = await MyToken.new(1000000);
+    });
+
     it("should put 1000000 tokens in the first account", async () => {
-        let instance = await MyToken.deployed();
+        let instance = await this.myToken;
         let totalSupply = await instance.totalSupply();
         expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
     });
 
     it("is not possible to send more tokens than available", async () => {
-        let instance = await MyToken.deployed();
+        let instance = await this.myToken;
         let balanceOfDeployer = await instance.balanceOf(deployerAccount);
         expect(instance.transfer(recipient, new BN(balanceOfDeployer+1))).to.eventually.be.rejected;
         expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(new BN(balanceOfDeployer));
@@ -31,7 +35,7 @@ contract("MyToken test", async accounts => {
 
     it("is possible to send tokens between accounts", async() => {
         const sendTokens = 1;
-        let instance = await MyToken.deployed();
+        let instance = await this.myToken;
         let totalSupply = await instance.totalSupply();
         expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(totalSupply);
         expect(instance.transfer(recipient, sendTokens)).to.eventually.be.a.fulfilled;
